38491a54e4f9bac8b7e9d76775790fbc05287f17
shinriyo <shinriyo@gmail.com>
2025-02-09 00:21:43 +0900

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.shinriyo"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
+        untilBuild.set("252.*")
     }
 
     signPlugin {
